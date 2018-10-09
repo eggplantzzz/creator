36150d719a5d01bdf762814a247f36822dec10e5
eggplantzzz <tmfeickert@gmail.com>
2018-10-09 09:43:36 -0400

    Remove some comments and clean up some code

--- client/src/App.js
+++ client/src/App.js
@@ -16,42 +16,18 @@ class App extends Component {
 
   componentDidMount = async () => {
     try {
-      // Get network provider and web3 instance.
       const web3 = await getWeb3();
-
-      // Use web3 to get the user's accounts.
       const accounts = await web3.eth.getAccounts();
 
-      // Get the contract instance.
       const Contract = truffleContract(PropertiesContract);
       Contract.setProvider(web3.currentProvider);
       const instance = await Contract.deployed();
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      this.setState({ web3, accounts, contract: instance });
     } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`
-      );
+      alert(`Failed to load web3, accounts, or contract. Check console for details.`);
       console.log(error);
     }
-  };
-
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    // console.log('about to set the head size');
-    // await contract.setHeadSize(5, { from: accounts[0] });
-    //
-    // // Get the value from the contract to prove it worked.
-    // const response = await contract.getHeadSize();
-    //
-    // // Update state with the result.
-    // console.log("the response --> ", JSON.stringify(response));
-    // this.setState({ headSize: response.toNumber() });
   };
 
   handleChange = (event) => {
